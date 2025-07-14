6f3ecbee31d0983b7693f4f8bab973420186de42
Prityush Parmar <89090802+prityushsingh@users.noreply.github.com>
2025-07-14 16:15:52 +0530

    Update Process String with Special Operations I(3612).py

--- Arrays-Hashing/Process String with Special Operations I(3612).py
+++ Arrays-Hashing/Process String with Special Operations I(3612).py
@@ -1,1 +1,23 @@
+class Solution(object):
+    def processStr(self, s):
+        res = []
+        for c in s:
+            if c == "*":
+                if res:
+                    res.pop()
+            elif c == "#":
+                res += res[:]
+            elif c == "%":
+                left = 0
+                right = len(res) - 1
+                while left < right:
+                    res[left] , res[right] = res[right] , res[left]
+                    left += 1
+                    right -= 1
+            else:
+                res.append(c)
+        return "".join(res)
 
+
+#Time complexity : O(N²) in the worst case due to repeated string concatenation and copying
+#Space complexity : O(N)
